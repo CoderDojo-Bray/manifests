af30dbfb85f8c5b73602a93ae3a8d4572bb60a58
Liam Friel <liam.friel@gmail.com>
2015-02-21 23:44:15 +0000

    updating

--- site.pp
+++ site.pp
@@ -23,6 +23,9 @@ node default {
     # Install the gnusasl components
     include gnusasl
     
+    # Include the augest components
+    include augeas
+    
     # install the jabber components
     include jabber
 
